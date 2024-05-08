691f8439c039161d19528bf8777be1cdd3404db4
Daniel <dwgrossberg@gmail.com>
2024-05-08 06:21:01 -0400

    Add static html files to hbs views. Add index.css and index.js files

--- app.js
+++ app.js
@@ -3,42 +3,61 @@
 /*
     SETUP
 */
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT = 1911;                 // Set a port number at the top so it's easy to change in the future
+var express = require("express"); // We are using the express library for the web server
+var app = express(); // We need to instantiate an express object to interact with the server in our code
+PORT = 1911; // Set a port number at the top so it's easy to change in the future
 
 //handlebars setup
 
-const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+const { engine } = require("express-handlebars");
+var exphbs = require("express-handlebars"); // Import express-handlebars
+app.engine(".hbs", engine({ extname: ".hbs" })); // Create an instance of the handlebars engine to process templates
+app.set("view engine", ".hbs"); // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 // Database
-var db = require('./database/db-connector')
+var db = require("./database/db-connector");
+
+// Public folder assets
+const path = require("path");
+app.use(express.static(path.join(__dirname, "/public")));
 
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-       res.render('index'); 
-    });                                         
+app.get(
+  "/",
+  function (
+    req,
+    res // This is the basic syntax for what is called a 'route'
+  ) {
+    res.render("index");
+  }
+);
 
-app.get('/departments', function(req, res)                 
-    {
-       res.render('departments'); 
-    });       
+app.get("/departments", function (req, res) {
+  res.render("departments");
+});
 
-app.get('/employees', function(req, res)                 
-    {
-       res.render('employees'); 
-    });                                    
+app.get("/employees", function (req, res) {
+  res.render("employees");
+});
 
+app.get("/certifications", function (req, res) {
+  res.render("certifications");
+});
+
+app.get("/training_sessions", function (req, res) {
+  res.render("training_sessions");
+});
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+app.listen(PORT, function () {
+  // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+  console.log(
+    "Express started on http://localhost:" +
+      PORT +
+      "; press Ctrl-C to terminate."
+  );
 });
---
+++ public/js/index.js
@@ -0,0 +1,120 @@
+// Departments
+
+function showDepartment(department) {
+  // Hide all department sections
+  var departmentSections = document.querySelectorAll(".department-section");
+  departmentSections.forEach(function (section) {
+    section.style.display = "none";
+  });
+
+  // Show the selected department section
+  var selectedDepartment = document.getElementById(department);
+  if (selectedDepartment) {
+    selectedDepartment.style.display = "block";
+  }
+}
+
+// Certifications
+
+function showCertForms(dowhat) {
+  var sections = ["browse", "insert", "update", "delete"];
+  sections.forEach(function (section) {
+    var sectionElement = document.getElementById(section);
+    if (dowhat === "all" || dowhat === section) {
+      sectionElement.style.display = "block"; // Show the section
+    } else {
+      sectionElement.style.display = "none"; // Hide the section
+    }
+  });
+}
+
+function newCertification() {
+  showCertForms("insert");
+}
+
+function updateCertification(element) {
+  var certID = element.getAttribute("data-id");
+  var certName = element.getAttribute("data-name");
+  var certOrg = element.getAttribute("data-org");
+  var description = element.getAttribute("data-description");
+
+  document.getElementById("updateCertID").value = certID;
+  document.getElementById("updateCertName").value = certName;
+  document.getElementById("updateCertOrg").value = certOrg;
+  document.getElementById("updateCertDescription").value = description;
+
+  showCertForms("update");
+}
+
+function deleteCertification(element) {
+  var certID = element.getAttribute("data-id");
+  var certName = element.getAttribute("data-name");
+  var certOrg = element.getAttribute("data-org");
+  var description = element.getAttribute("data-description");
+
+  document.getElementById("deleteCertID").value = certID;
+  document.getElementById("deleteCertName").value = certName;
+  document.getElementById("deleteCertOrg").value = certOrg;
+  document.getElementById("deleteCertDescription").value = description;
+
+  showCertForms("delete");
+}
+
+function browseCertifications() {
+  showCertForms("browse");
+}
+
+function showCertAll() {
+  showCertForms("all");
+}
+
+// Employees
+
+function showform(dowhat) {
+  var sections = ["browse", "insert", "update", "delete"];
+  sections.forEach(function (section) {
+    var sectionElement = document.getElementById(section);
+    if (dowhat === "all" || dowhat === section) {
+      sectionElement.style.display = "block"; // Show the section
+    } else {
+      sectionElement.style.display = "none"; // Hide the section
+    }
+  });
+}
+function newEmployee() {
+  showform("insert");
+}
+
+function updateEmployee(element) {
+  var id = element.getAttribute("data-id");
+  var fname = element.getAttribute("data-fname");
+  var lname = element.getAttribute("data-lname");
+  var email = element.getAttribute("data-email");
+  var deptid = element.getAttribute("data-deptid");
+
+  document.getElementById("updateEmployeeID").value = id;
+  document.getElementById("updateEmployeeIDSpan").textContent = id;
+  document.getElementById("updateEmployeeFName").value = fname;
+  document.getElementById("updateEmployeeLName").value = lname;
+  document.getElementById("updateEmployeeEmail").value = email;
+  document.getElementById("updateEmployeeDeptID").value = deptid;
+
+  showform("update");
+}
+function deleteEmployee(element) {
+  var id = element.getAttribute("data-id");
+  var name = element.getAttribute("data-name");
+
+  document.getElementById("deleteEmployeeID").value = id;
+  document.getElementById("deleteEmployeeIDSpan").textContent = id;
+  document.getElementById("deleteEmployeeNameSpan").textContent = name;
+
+  showform("delete");
+}
+function browseEmployees() {
+  showform("browse");
+}
+
+function showAll() {
+  showform("all");
+}
