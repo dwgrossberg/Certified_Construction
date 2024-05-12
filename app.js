aa9ade2d15298bc489c6f4e32855470eb5da0c20
ditrungduong <diduongtrung@gmail.com>
2024-05-12 15:45:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 var express = require("express"); // We are using the express library for the web server
 var app = express(); // We need to instantiate an express object to interact with the server in our code
-const PORT = process.env.PORT || 1911;
+PORT = 1911; // Set a port number at the top so it's easy to change in the future
 
 //handlebars setup
 
@@ -24,12 +24,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 /*
     ROUTES
 */
-app.get(
-  "/",
-  function (
-    req,
-    res // This is the basic syntax for what is called a 'route'
-  ) {
+app.get("/", function (req,res) {
     res.render("index");
   }
 );
@@ -50,12 +45,12 @@ app.get("/training_sessions", function (req, res) {
   res.render("training_sessions");
 });
 
-app.get("/employee_certifications", function (req, res) {
-  res.render("employee_certifications");
+app.get("/employees_cert", function (req, res) {
+  res.render("employees_cert");
 });
-
-// Heroku deployment
-
+app.get("/employees_train", function (req, res) {
+  res.render("employees_train");
+});
 /*
     LISTENER
 */
