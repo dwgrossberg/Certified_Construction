aef56595c9aeea5d6c7bfefca6ea4c38f07045f9
Daniel <dwgrossberg@gmail.com>
2024-05-12 15:50:09 -0400

    Setup Heroku auto deploy from GitHub

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 var express = require("express"); // We are using the express library for the web server
 var app = express(); // We need to instantiate an express object to interact with the server in our code
-PORT = 1911; // Set a port number at the top so it's easy to change in the future
+const PORT = process.env.PORT || 1911;
 
 //handlebars setup
 
@@ -53,6 +53,8 @@ app.get("/training_sessions", function (req, res) {
 app.get("/employee_certifications", function (req, res) {
   res.render("employee_certifications");
 });
+
+// Heroku deployment
 
 /*
     LISTENER
