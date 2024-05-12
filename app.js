685f2d40f121766e7474861180cebfa88be12993
ditrungduong <diduongtrung@gmail.com>
2024-05-12 15:49:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 var express = require("express"); // We are using the express library for the web server
 var app = express(); // We need to instantiate an express object to interact with the server in our code
-PORT = 1911; // Set a port number at the top so it's easy to change in the future
+const PORT = process.env.PORT || 1911; // Set a port number at the top so it's easy to change in the future
 
 //handlebars setup
 
@@ -51,6 +51,8 @@ app.get("/employees_cert", function (req, res) {
 app.get("/employees_train", function (req, res) {
   res.render("employees_train");
 });
+// Heroku deployment
+
 /*
     LISTENER
 */
