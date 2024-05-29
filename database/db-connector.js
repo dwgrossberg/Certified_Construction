900b68d5df98b09895b9d63e7ae31d6d3f8bcd89
Daniel Grossberg <dwgrossberg@gmail.com>
2024-05-29 06:59:14 -0700

    Add creds to .env file

--- database/db-connector.js
+++ database/db-connector.js
@@ -4,27 +4,14 @@ var mysql = require('mysql');
 
 // Define credentials for both users
 const credentials = {
-    grossbed: {
-        connectionLimit: 10,
-         host: 'classmysql.engr.oregonstate.edu',
-         user: 'cs340_grossbed',
-         password: '7102',
-         database: 'cs340_grossbed'
-     },
-     duongdi: {
          connectionLimit: 10,
-         host: 'classmysql.engr.oregonstate.edu',
-         user: 'cs340_duongdi',
-         password: '0749',
-         database: 'cs340_duongdi'
-     }
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER_NAME,
+         password: process.env.DB_PWD,
+         database: process.env.DB_USER_NAME
  };
 
-
-var dbUser = process.env.DB_USER || 'duongdi'; // Default to 'duongdi' if not set
-
-
-var pool = mysql.createPool(credentials[dbUser]);
+var pool = mysql.createPool(credentials);
 
 // Export it for use in our application
 module.exports.pool = pool;
